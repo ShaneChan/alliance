5ac2520f4fde4b926e92b535e79bb3a55e720fb6
chenzhihang <chenzhihang@taifengcd.cn>
2022-11-17 09:30:38 +0800

    使用暂时的字符串信息

--- AllianceServer/predefine/msg.go
+++ AllianceServer/predefine/msg.go
@@ -1,18 +1,18 @@
 package predefine
 
 var MsgFlags = map[int]string{
-	SUCCESS:                            "成功",
-	NOT_LOGIN:                          "当前未登录",
-	REGISTER_FAILED:                    "注册失败",
-	LOGIN_FAILED:                       "登录失败",
-	GET_ALLIANCE_LIST_FALIED:           "获取公会列表失败",
-	CREATE_ALLIANCE_FAILED:             "创建公会失败",
-	JOIN_ALLIANCE_FALIED:               "加入公会失败",
-	LEAVE_ALLIANCE_FALILED:             "解散公会失败",
-	DESTROY_ALLIANCE_ITEM_FALIED:       "销毁公会物品失败",
-	TIDY_ALLIANCE_ITEM_FALIED:          "整理公会物品失败",
-	COMMIT_ALLIANCE_ITEM_FALIED:        "提交公会物品失败",
-	INCREASE_ALLIANCE_CAPACITY_FALILED: "扩容公会仓库失败",
+	SUCCESS:                            "aa",
+	NOT_LOGIN:                          "bb",
+	REGISTER_FAILED:                    "cc",
+	LOGIN_FAILED:                       "dd",
+	GET_ALLIANCE_LIST_FALIED:           "ee",
+	CREATE_ALLIANCE_FAILED:             "ff",
+	JOIN_ALLIANCE_FALIED:               "gg",
+	LEAVE_ALLIANCE_FALILED:             "hh",
+	DESTROY_ALLIANCE_ITEM_FALIED:       "ww",
+	TIDY_ALLIANCE_ITEM_FALIED:          "xx",
+	COMMIT_ALLIANCE_ITEM_FALIED:        "zz",
+	INCREASE_ALLIANCE_CAPACITY_FALILED: "qq",
 }
 
 func GetMsg(code int) string {
