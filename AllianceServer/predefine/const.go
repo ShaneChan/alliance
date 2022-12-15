14364e8089d9a8a05de7a7419b80e60499142879
chenzhihang <chenzhihang@taifengcd.cn>
2022-12-15 09:33:19 +0800

    新增const信息

--- AllianceServer/predefine/const.go
+++ AllianceServer/predefine/const.go
@@ -1,6 +1,7 @@
 package predefine
 
 const (
+	CONSUME_GOLD          = 1  // 每次升格子消耗的金币数量
 	ITEM_MAX_NUM_ONE_GRID = 5  // 一个格子的最大道具数量
 	INITIAL_MAX_GRID_NUM  = 30 // 初始最大格子数
 	INCREASE_NUM          = 10 // 一次性扩容的数量
